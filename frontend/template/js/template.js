7f96f5945e5983a275ddc08f2788ce6cfe007af5
hamitmizrak <hamitmizrak@gmail.com>
2023-02-05 13:13:44 +0300

    back to top

--- frontend/template/js/template.js
+++ frontend/template/js/template.js
@@ -3,15 +3,15 @@
 //#########################################################
 //NAVBAR
 //darkLight
-let darkLight=()=>{
+let darkLight = () => {
     document.body.classList.toggle("dark_mode");
 }
 
 //TAGS
-$(document).ready(function(){
-    const apiData=["adana","bursa","ceyhan","malatya","istanbul","izmir","diyarbakır","elazığ"];
+$(document).ready(function () {
+    const apiData = ["adana", "bursa", "ceyhan", "malatya", "istanbul", "izmir", "diyarbakır", "elazığ"];
     $("#tags").autocomplete({
-        source:apiData
+        source: apiData
     });
 });
 
@@ -19,5 +19,15 @@ $(document).ready(function(){
 //#########################################################
 // FOOTER 
 
-document.getElementById("next_year").innerHTML=new Date().getFullYear();
+//Date
+document.getElementById("next_year").innerHTML = new Date().getFullYear();
 // $("#next_year").html(new Date().getFullYear());
+
+// back to top
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $("#back_top").fadeIn("slow");
+    } else {
+        $("#back_top").fadeOut("slow");
+    }
+});
