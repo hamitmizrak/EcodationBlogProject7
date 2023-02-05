79a7e4dc7db4578f63e5a45becfddf16cc0c5083
hamitmizrak <hamitmizrak@gmail.com>
2023-02-05 09:40:51 +0300

    Frontend Tutorials

--- src/main/java/com/hamitmizrak/ecodationblogproject7/EcodationBlogProject7Application.java
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/EcodationBlogProject7Application.java
@@ -2,12 +2,35 @@ package com.hamitmizrak.ecodationblogproject7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+// Mongo aktif etmek icin
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+//
+// Asenkron açmak icin
+// @EnableAsync
+
+// Spring Boot Cache mekanizmasını aktif ediyorum
+// @EnableCaching
+
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
+        //org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        //org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
+//@SpringBootApplication
 public class EcodationBlogProject7Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcodationBlogProject7Application.class, args);
-	}
+    public static void main(String[] args) {
 
+        //Disables headless JOptionPane
+        System.setProperty("java.awt.headless", "false");
+
+        SpringApplication.run(EcodationBlogProject7Application.class, args);
+    }
 }
