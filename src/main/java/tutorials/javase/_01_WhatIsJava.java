eb3199dcb6ed8cf36d2dda1b6deecef092eb38bd
hamitmizrak <hamitmizrak@gmail.com>
2023-02-18 10:32:43 +0300

    java se tutorials

---
+++ src/main/java/tutorials/javase/_01_WhatIsJava.java
@@ -0,0 +1,10 @@
+package tutorials.javase;
+/*multi line comment*/
+//single comment
+public class _01_WhatIsJava {
+    //psvm ==>TAB
+    public static void main(String[] args) {
+    //sout ==>TAB
+        System.out.println("Hello World! ");
+    } //end psvm
+} //end class
