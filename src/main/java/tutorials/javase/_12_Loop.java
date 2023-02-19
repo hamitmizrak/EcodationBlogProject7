77ac36353ca1fc49475c0e0728719291820ed894
hamitmizrak <hamitmizrak@gmail.com>
2023-02-19 12:05:00 +0300

    Date,Calendar,LocalDate

--- src/main/java/tutorials/javase/_12_Loop.java
+++ src/main/java/tutorials/javase/_12_Loop.java
@@ -21,11 +21,13 @@ public class _12_Loop {
         // ÖDEV
         // kullanıcıdan alınan bir değerin sayı mı? harf mi ? özel simge mi ?
 
+        // ////////////////////////////////////////////////////////////////////////////////////
         // Biz : sayı tahmin oyunu ?
         // kullanıcıdan aldığımız bir sayı(Scanner veya jOptionalPnane) ile bilgisayarın 1-10 arasında sayı üretsin (Random Object)
         // bilgisayarın ürettiği sayıyı tahmin etmeye çalışalım.
 
         // sayı tahmin sayımız 3 defa olmalıdır
+        // Oyuna başlama saatini göstersin. ve oyunda toplam kaldığımız süreyi hesaplayı ekranda göstersin.
         // Ekranda kaçıncı tahminde bulduğumuzu bize söylesin.
         // Eğer doğru tahmin edersek, bize soru sorsun oyunu tekrar oynamak istiyor musunuz ?
         // eğer cevabımız evetse oyuna tekrar oynatsın. eğer hayırsa şimdiye kadar bu oyunu kaç kere oynadığımızı bize söylesin.
@@ -61,5 +63,23 @@ public class _12_Loop {
             m++;
         }while(m<=10);
         System.out.println(sum2);
+
+        // ÖDEV:
+        // Vize Final Ödevi
+        // kullanıcıdan alınan vize ve final notuna göre geçme(ortalama) Algoritması
+        // eğer kullanıcı vize veya final 0(sıfır) bir değer girerse sistemden atsın
+        // ekranda "Sifir disiplin hatasi Sistemden cikiliyor"
+        // bunun haricinde sürekli vize final sorsun
+        // Not Harfleri ==> AA BA BB FF
+        // not ortalaması: ortalama<50 altında ise kaldı FF
+        // not ortalaması: ortalama==50 Geçti
+        // not ortalaması: 55<=x<=69 BB
+        // not ortalaması: 70<=x<=84 BA
+        // not ortalaması: 85<=x<=100 AA
+        // Vize:40% Final:60%
+        // Dikkat: Consolda virgül kullanmalısın.
+        // while sonsuz döngü   ==> while(true) {}
+        // for sonsuz döngü     ==> for(;;) {}
+
     }
 }
---
+++ src/main/java/tutorials/javase/_13_DateCalendarLocalDate.java
@@ -0,0 +1,36 @@
+package tutorials.javase;
+
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
+public class _13_DateCalendarLocalDate {
+
+    public static void main(String[] args) {
+        //Date: 1900 , 1Ocak 1970
+
+        //DATE
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(date.getDay()); //pazar=0 pazartesi:1 .. cumartesi:6 ,aylar: sıfırdan başlar saymaya
+        System.out.println(date.getDate());
+        System.out.println(date.getYear() + 1900);
+        System.out.println(date.getMonth());//aylar: sıfırdan başlar saymaya
+        date.setMonth(10);
+        System.out.println(date.getMonth());
+        System.out.println(date.getHours()+":"+date.getMinutes());
+        System.out.println(new Date(System.currentTimeMillis()) );
+
+        //Calendar:
+        Calendar calendar=Calendar.getInstance();
+        System.out.println(calendar.getTime());
+        System.out.println(calendar.getTime().getYear()+1900);
+
+        //LocalDate
+        LocalDate localDate=LocalDate.now();
+        System.out.println(localDate);
+        System.out.println(localDate.getYear());
+        System.out.println(localDate.getMonth());
+        System.out.println(localDate.getDayOfMonth());
+    }
+}
