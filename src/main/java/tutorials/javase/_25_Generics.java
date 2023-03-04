eeaa54f3040b48e02d39a4ec4671a2aa326071f1
hamitmizrak <hamitmizrak@gmail.com>
2023-03-04 09:50:13 +0300

    Generics

---
+++ src/main/java/tutorials/javase/_25_Generics.java
@@ -0,0 +1,42 @@
+package tutorials.javase;
+
+import lombok.Data;
+
+@Data
+public class _25_Generics <T>{
+
+    //field
+    private long id;
+    private T name;
+
+    //constructor
+    public _25_Generics() {
+    }
+    public _25_Generics(long id, T name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void isnotGenericsMethod(String name){
+        System.out.println(name);
+    }
+
+    //generics method
+    public <K> void genericsMethod(K name){
+        System.out.println(name);
+    }
+
+}
+
+class MainGenericsData{
+    public static void main(String[] args) {
+        _25_Generics generics=new _25_Generics(1,true );
+        System.out.println(generics.getId()+" "+generics.getName());
+
+        generics.isnotGenericsMethod("Merhabalar");
+        generics.genericsMethod("Diğer");
+    }
+}
+
+
+
