79759ae19fbdd7c2c4a75783000ae41b78f6259a
hamitmizrak <hamitmizrak@gmail.com>
2023-03-11 12:14:10 +0300

    Spring MVC thymeleaf0

--- src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/_01_Thymeleaf1.java
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/_01_Thymeleaf1.java
@@ -1,4 +1,17 @@
 package com.hamitmizrak.ecodationblogproject7.springboot._02_SpringMVC;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class _01_Thymeleaf1 {
+
+
+    // http://localhost:4444/thymeleaf0
+    @GetMapping("/thymeleaf0")
+    @ResponseBody
+    public String getThymeleaf0(){
+        return "Merhabalar ben htmlsiz görüntüyüm";
+    }
 }
