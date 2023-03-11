2537e48c19429a63e48b0634ee4ead6634bc645f
hamitmizrak <hamitmizrak@gmail.com>
2023-03-11 12:18:38 +0300

    Spring MVC thymeleaf0

--- src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/_01_Thymeleaf1.java
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/_01_Thymeleaf1.java
@@ -7,10 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class _01_Thymeleaf1 {
 
-
     // http://localhost:4444/thymeleaf0
-    @GetMapping("/thymeleaf0")
-    @ResponseBody
+    @GetMapping("/thymeleaf0")// @get: url çağırma
+    @ResponseBody//Bir html sayfası olmadan direk göstersin
     public String getThymeleaf0(){
         return "Merhabalar ben htmlsiz görüntüyüm";
     }
