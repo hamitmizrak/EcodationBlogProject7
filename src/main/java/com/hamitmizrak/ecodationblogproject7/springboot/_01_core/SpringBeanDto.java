43c8ec32b30165a3c4191dd7028b9892494854a4
hamitmizrak <hamitmizrak@gmail.com>
2023-03-11 11:49:54 +0300

    @Bean
    @PostConstruct

---
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_01_core/SpringBeanDto.java
@@ -0,0 +1,18 @@
+package com.hamitmizrak.ecodationblogproject7.springboot._01_core;
+
+//bean:
+
+import lombok.*;
+import lombok.extern.log4j.Log4j2;
+
+//lombok
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Log4j2
+@Builder
+@ToString
+public class SpringBeanDto {
+    private Long id;
+    private String beanName;
+}
---
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_01_core/SpringBootBean.java
@@ -0,0 +1,35 @@
+package com.hamitmizrak.ecodationblogproject7.springboot._01_core;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringBootBean {
+
+    //SF:Spring Frameowork
+    //SF bir parçası olması için @Bean annotaiton yazıyoruz.
+
+    @Bean
+    public SpringBeanDto springBeanDto(){
+        return SpringBeanDto.builder()
+                .id(1L).beanName("bean adı 44")
+                .build();
+    }
+
+    //parametresiz constructor
+    public  SpringBootBean(){
+        System.out.println("parametresiz constructor");
+    }
+
+    //Constructor
+    @PostConstruct
+    public void init(){
+        System.out.println("@PostConstruct constructor");
+    }
+
+    public static void main(String[] args) {
+        SpringBootBean  bootBean=new SpringBootBean();
+        System.out.println(bootBean);
+    }
+}
