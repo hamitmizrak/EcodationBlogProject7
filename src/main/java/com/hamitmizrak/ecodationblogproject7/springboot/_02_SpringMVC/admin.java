54a2c0549e2079144db90566a2955227c9a7e86c
hamitmizrak <hamitmizrak@gmail.com>
2023-03-12 11:53:44 +0300

    admin dataTables

--- src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/admin.java
+++ src/main/java/com/hamitmizrak/ecodationblogproject7/springboot/_02_SpringMVC/admin.java
@@ -4,10 +4,6 @@ import com.hamitmizrak.ecodationblogproject7.springboot.AdminDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
